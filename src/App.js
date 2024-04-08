cd6e3f2d45a04c6553da496a8ec3d94628855b21
OmarGeno <106829277+OmarGeno@users.noreply.github.com>
2024-04-09 02:30:36 +0300

    resume project update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import "./App.css";
 import { AnimatedRoutes } from "./AnimatedRoutes";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6FZpO0rgZng0u_1sFvoxkRPKHGUr5VR8",
+  authDomain: "omar-altaha.firebaseapp.com",
+  projectId: "omar-altaha",
+  storageBucket: "omar-altaha.appspot.com",
+  messagingSenderId: "535109324226",
+  appId: "1:535109324226:web:66f632bfc1db11f81d1a5b"
+};
+
+const app = initializeApp(firebaseConfig);
 
 const App = () => {
   return (
--- src/components/Resume Body/Navbar/ResumeIconList.jsx
+++ src/components/Resume Body/Navbar/ResumeIconList.jsx
@@ -1,41 +1,40 @@
-import './ResumeCollapseList.css'
+import "./ResumeCollapseList.css";
 
-const ResumeIconList= () => {
+const ResumeIconList = () => {
   return (
     <div className="icon-container">
-    <div className="icon-tags">
-      <a
-        href="https://www.linkedin.com/in/omar-al-taha-4078781b4/"
-        target="_blank" rel="noreferrer"
-      >
-        <img
-          src="https://img.icons8.com/color/512/linkedin-2.png"
-          alt=""
-        />
-      </a>
-    </div>
-    <div className="icon-tags">
-      <a href="https://github.com/OmarGeno" target="_blank" rel="noreferrer">
-        <img src="https://img.icons8.com/nolan/512/github.png" alt="" />
-      </a>
-    </div>
-    <div className="icon-tags">
-      <button className=" btn">
-        <img
-          src="https://img.icons8.com/plasticine/512/contract-job.png"
-          alt=""
-        />
+      <div className="icon-tags">
         <a
-          href="https://drive.google.com/uc?id=1o1860qvWal-qX67XnQOyGTFr8i2_M1sC&export=download"
-          target="_blank" rel="noreferrer"
-          download
+          href="https://www.linkedin.com/in/omar-al-taha-4078781b4/"
+          target="_blank"
+          rel="noreferrer"
         >
-          Download resume
+          <img src="https://img.icons8.com/color/512/linkedin-2.png" alt="" />
         </a>
-      </button>
+      </div>
+      <div className="icon-tags">
+        <a href="https://github.com/OmarGeno" target="_blank" rel="noreferrer">
+          <img src="https://img.icons8.com/nolan/512/github.png" alt="" />
+        </a>
+      </div>
+      <div className="icon-tags">
+        <button className=" btn">
+          <img
+            src="https://img.icons8.com/plasticine/512/contract-job.png"
+            alt=""
+          />
+          <a
+            href="https://drive.usercontent.google.com/u/0/uc?id=1wlS8AFZ3-biPPDJL5hIUWdFq-gTThbWP&export=download"
+            target="_blank"
+            rel="noreferrer"
+            download
+          >
+            Download resume
+          </a>
+        </button>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default ResumeIconList
+export default ResumeIconList;
