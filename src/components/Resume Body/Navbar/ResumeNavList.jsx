3c1e7b8b5e35132fe00a84f223778370166635a8
OmarGeno <106829277+OmarGeno@users.noreply.github.com>
2024-04-09 06:13:53 +0300

    new update

--- src/components/Resume Body/Navbar/ResumeNavList.jsx
+++ src/components/Resume Body/Navbar/ResumeNavList.jsx
@@ -1,35 +1,47 @@
-import './ResumeCollapseList.css'
-import { Link, Outlet } from 'react-router-dom'
+import "./ResumeCollapseList.css";
+import { Link, Outlet } from "react-router-dom";
 
 const ResumeNavList = () => {
   const omar = {
-    about: 'About me',
-    work: 'Projects',
-    skills: 'My skills',
-    contact: 'Contact',
-  }
+    about: "About me",
+    work: "Projects",
+    skills: "My skills",
+    contact: "Contact",
+  };
   return (
     <>
       <nav className="resume-collapse__nav">
-        <div>
-          <Link className='links-nav' to="/about" smooth={+true} offset={50} duration={500}>
-            {omar.about}
-          </Link>
-        </div>
-        <div>
-          <Link className='links-nav' to="/skills" smooth={+true} offset={50} duration={500}>
-            {omar.skills}
-          </Link>
-        </div>
-        <div>
-          <Link className='links-nav' to="/contact" smooth={+true} offset={50} duration={500}>
-            {omar.contact}
-          </Link>
-        </div>
+        <Link
+          className="links-nav"
+          to="/about"
+          smooth={+true}
+          offset={50}
+          duration={500}
+        >
+          {omar.about}
+        </Link>
+        <Link
+          className="links-nav"
+          to="/skills"
+          smooth={+true}
+          offset={50}
+          duration={500}
+        >
+          {omar.skills}
+        </Link>
+        <Link
+          className="links-nav"
+          to="/contact"
+          smooth={+true}
+          offset={50}
+          duration={500}
+        >
+          {omar.contact}
+        </Link>
       </nav>
-      <Outlet/>
+      <Outlet />
     </>
-  )
-}
+  );
+};
 
-export default ResumeNavList
+export default ResumeNavList;
